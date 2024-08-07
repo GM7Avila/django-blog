979b3c33b82e5ee1ff82306bf20f3412be120703
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-08-07 18:32:08 -0300

    feat: View - Creating templates and setting up the basic structure of the blog

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
 
+posts = [
+  {
+    'author': 'GM7Avila',
+    'title': 'My Linux Setup',
+    'subtitle': "All my configurations and settings that I use on my Linux Mint",
+    'content': 'First post content',
+    'date_posted': 'August 7, 2024'
+  },
+  {
+    'author': 'GM7Avila',
+    'title': 'Hello World!',
+    'subtitle': "My first post!",
+    'content': 'Second post content',
+    'date_posted': 'August 7, 2024'
+  }
+]
+
 # Create your views here.
 def home(request):
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts
+  }
+
+  return render(request, 'blog/home.html', context)
 
 def about(request):
-  return render(request, 'blog/about.html')
+  return render(request, 'blog/about.html', {'title': 'About'})
