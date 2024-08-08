7c18d8905072a194657632480780bea0e97e10f0
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-08-07 21:35:24 -0300

    feat: Model - Database and Migrations (User, Post)

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-  {
-    'author': 'GM7Avila',
-    'title': 'My Linux Setup',
-    'subtitle': "All my configurations and settings that I use on my Linux Mint",
-    'content': 'First post content',
-    'date_posted': 'August 7, 2024'
-  },
-  {
-    'author': 'GM7Avila',
-    'title': 'Hello World!',
-    'subtitle': "My first post!",
-    'content': 'Second post content',
-    'date_posted': 'August 7, 2024'
-  }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
 
   return render(request, 'blog/home.html', context)
