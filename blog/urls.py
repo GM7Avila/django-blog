36d6d2ec4c1e95594e048b8a32660ca118553e13
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-08-09 20:51:13 -0300

    refactor: auth urls

--- blog/urls.py
+++ blog/urls.py
@@ -7,8 +7,4 @@ from users import views as users_views
 urlpatterns = [
   path('', views.home, name = 'blog-home'),
   path('about/', views.about, name = "blog-about"),
-
-  path('signup/', users_views.register, name = 'register'),
-  path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'login'),
-  path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name = 'logout'),
 ]
