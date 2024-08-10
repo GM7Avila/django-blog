ccf67345d51e85ff2929bd8f4cb6f89258fde0d5
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-08-10 01:18:05 -0300

    feat: Update User Profile

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -14,3 +15,15 @@ class UserRegisterForm(UserCreationForm):
         # removendo a opção de Password-based authentication
         if 'usable_password' in self.fields:
             del self.fields['usable_password']
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username']
+
+class ProfileUpdateForm(forms.ModelForm):
+
+  class Meta:
+    model = Profile
+    fields = ['bio', 'image']
+
