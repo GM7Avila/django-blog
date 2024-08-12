481a60970bf8074968dc55937e8f20b07d0dd815
GM7Avila <guilherme.medeiros.avila@gmail.com>
2024-08-11 21:37:34 -0300

    style: home and user page

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView  # CRUD
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.core.paginator import Paginator
+from django.contrib.auth.models import User
 from .models import Post
 
 class PostListView(ListView):
@@ -10,6 +12,23 @@ class PostListView(ListView):
   context_object_name = 'posts'
   ordering = ['-date_posted']
   paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' 
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 10
+
+    def get_queryset(self):
+        self.user_profile = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=self.user_profile).order_by('-date_posted')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_profile'] = self.user_profile
+        return context
+
 
 class PostDetailView(DetailView):
   model = Post
